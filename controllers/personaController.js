1d2c8140de1cf81e4154b84f4bdd4858632087de
Fede <84208153+Federicoluna1@users.noreply.github.com>
2021-06-10 14:22:16 -0300

    personaController file

---
+++ controllers/personaController.js
@@ -0,0 +1,38 @@
+const personaService = require('../services/personaService');
+
+module.exports = {
+    guardarUnaPersona: async function(persona) {
+        var personaNueva = personaService.guardarUnaPersona(persona);
+        return personaNueva;
+    },
+    listarPersonas: async function() {
+        var listado = await personaService.listarPersonas();
+        return listado;
+    },
+    buscarUnaPersona: async function(id) {
+        var persona = personaService.traerUnaPersona(id);
+        return persona;
+    },
+    modificarPersona: async function(id, edad, mail) {
+        var persona = null;
+
+        var resultado = await personaService.modificarPersona(id, edad, mail);
+
+        if (resultado) {
+            persona = await persona.personaService.traerUnaPersona(id);
+        }
+
+        return persona;
+    },
+    borrarPersona: async function(id) {
+        var persona = null;
+
+        var resultado = await personaService.borrarPersona(id);
+
+        if (resultado) {
+            persona = await persona.personaService.traerUnaPersona(id);
+        }
+
+        return persona;
+    },
+}
