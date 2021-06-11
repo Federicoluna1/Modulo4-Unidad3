7cad60c55a5a2f66dfb975af5b5e66da61371134
Fede <84208153+Federicoluna1@users.noreply.github.com>
2021-06-11 11:06:50 -0300

    Updated

--- models/persona.js
+++ models/persona.js
@@ -2,27 +2,27 @@ const qy = require('../database');
 
 module.exports = {
     guardarUnaPersona: async function(persona) {
-        var result = await conexion.query(
+        var result = await qy(
             "INSERT INTO persona (nombre, apellido, nickname, edad, email) VALUES (?,?,?,?,?)", [persona.nombre, persona.apellido, persona.nickname, persona.edad, persona.email]);
         return result.insertId;
     },
     buscarUnaPersona: async function(id) {
-        var unaPersona = await conexion.query(
+        var unaPersona = await qy(
             "SELECT * FROM persona WHERE id = ?", [id]);
         return unaPersona[0];
     },
     buscarTodasLasPersonas: async function() {
-        var listadoPersonas = await conexion.query("SELECT * FROM persona");
+        var listadoPersonas = await qy("SELECT * FROM persona");
         return listadoPersonas;
     },
     modificarPersona: async function(id, edad, mail) {
-        var result = await conexion.query(
+        var result = await qy(
             "UPDATE persona SET edad = ?, email = ? WHERE id = ?", [edad, email, id]);
         return result.changedRows;
     },
     borrarPersona: async function(id) {
         var fecha = new Date();
-        var result = await conexion.query("UPDATE persona SET deleted = ?, date_deleted = ?", [true, fecha]);
+        var result = await qy("UPDATE persona SET deleted = ?, date_deleted = ?", [true, fecha]);
         return result.affectedRows;
     },
 }
