8f05aebc37ff2e1bdb009f6effa4537e3892e863
Fede <84208153+Federicoluna1@users.noreply.github.com>
2021-06-12 12:59:22 -0300

    personaController.js up to the date

--- controllers/personaController.js
+++ controllers/personaController.js
@@ -10,18 +10,17 @@ module.exports = {
         return listado;
     },
     buscarUnaPersona: async function(id) {
-        var persona = personaService.traerUnaPersona(id);
+        var persona = personaService.buscarUnaPersona(id);
         return persona;
     },
-    modificarPersona: async function(id, edad, mail) {
+    modificarPersona: async function(id, edad, email) {
         var persona = null;
 
-        var resultado = await personaService.modificarPersona(id, edad, mail);
+        var resultado = await personaService.modificarPersona(id, edad, email);
 
         if (resultado) {
-            persona = await persona.personaService.traerUnaPersona(id);
+            persona = await persona.personaService.buscarUnaPersona(id);
         }
-
         return persona;
     },
     borrarPersona: async function(id) {
@@ -30,7 +29,7 @@ module.exports = {
         var resultado = await personaService.borrarPersona(id);
 
         if (resultado) {
-            persona = await persona.personaService.traerUnaPersona(id);
+            persona = await persona.personaService.buscarUnaPersona(id);
         }
 
         return persona;
